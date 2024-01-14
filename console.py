e3b3f3ef89f00cc24702b2297b8753dbfd96167d
MemanAwad <138756696+MemanAwad@users.noreply.github.com>
2024-01-14 23:50:30 +0300

    HBNB Command console

--- console.py
+++ console.py
@@ -21,7 +21,7 @@ class HBNBCommand(cmd.Cmd):
     """
     prompt = "(hbnb) "
     classes = ["BaseModel", "User", "Amenity",
-            "Place", "Review", "State", "City"]
+               "Place", "Review", "State", "City"]
 
     def do_EOF(self, line):
         """Quit command to exit the program"""
