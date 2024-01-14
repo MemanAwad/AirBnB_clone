e9aca234ee3330ed65878d845d1aa050291b90e5
MemanAwad <138756696+MemanAwad@users.noreply.github.com>
2024-01-15 00:28:43 +0300

    State Class

--- models/state.py
+++ models/state.py
@@ -1,6 +1,12 @@
 #!usr/bin/python3
+"""
+Module for State Class
+"""
 from models.base_model import BaseModel
 
-class State(BaseModel):
 
-    name  = ""
+class State(BaseModel):
+    """
+    Representation of State
+    """
+    name = ""
