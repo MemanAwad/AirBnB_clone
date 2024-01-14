99c24e78a59919bdfc4e30b777651ff67ffaee8a
Eilla-7 <eilafadel2238@gmail.com>
2024-01-14 13:22:58 -0800

    Review Class

--- models/review.py
+++ models/review.py
@@ -1,8 +1,15 @@
 #!usr/bin/python3
+"""
+Module for Review Class
+"""
 from models.base_model import BaseModel
 
-class Review(BaseModel):
 
-    place_id  = ""
+class Review(BaseModel):
+    """
+    Representation of Review
+    """
+
+    place_id = ""
     user_id = ""
     text = ""
