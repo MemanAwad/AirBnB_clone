2101db0ce142c62a1e1fb9340fab51d52cd5823b
Eilla-7 <eilafadel2238@gmail.com>
2024-01-14 06:07:59 -0800

    Base Model

--- models/base_model.py
+++ models/base_model.py
@@ -8,8 +8,11 @@ import models
 
 
 class BaseModel():
+    """Define Base Model"""
 
     def __init__(self, *args, **kwargs):
+        """Constructor"""
+
         date_format = "%Y-%m-%dT%H:%M:%S.%f"
         self.id = str(uuid4())
         self.created_at = datetime.utcnow()
@@ -28,14 +31,24 @@ class BaseModel():
         models.storage.new(self)
 
     def __str__(self):
+        """ print: [<class name>] (<self.id>) <self.__dict__>"""
+
         return "[{}] ({}) {}".format(self.__class__.__name__,
                 self.id, (self.__dict__))
 
     def save(self):
+        """updates the public instance attribute
+        updated_at with the current datetime
+        """
+
         self.updated_at = (datetime.utcnow())
         models.storage.save()
 
     def to_dict(self):
+        """returns a dictionary containing all keys/values
+        of __dict__ of the instance
+        """
+
         dict_user = self.__dict__.copy()
         dict_user["created_at"] = self.created_at.isoformat()
         dict_user["updated_at"] = self.updated_at.isoformat()
