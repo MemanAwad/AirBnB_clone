9586a73d573db8bbd950da9dc7b1b9a399712bde
MemanAwad <meman1awad@gmail.com>
2024-04-01 02:21:56 +0200

    fix

--- models/base_model.py
+++ models/base_model.py
@@ -1,32 +1,57 @@
 #!/usr/bin/python3
-""" class BaseModel that defines attributes/methods for other classes"""
+"""
+module of Base Model class
+"""
 
-from uuid import uuid4
 from datetime import datetime
+from uuid import uuid4
+import models
 
 
 class BaseModel:
-    """defines all common attributes/methods for other classes"""
+    """Define Base Model"""
 
-    def __init__(self):
-        """constructor"""
+    def __init__(self, *args, **kwargs):
+        """Constructor"""
+
+        date_format = "%Y-%m-%dT%H:%M:%S.%f"
         self.id = str(uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        self.created_at = datetime.utcnow()
+        self.updated_at = datetime.utcnow()
+
+        if kwargs:
+
+            kwargs.pop("__class__", None)
+            for key, value in kwargs.items():
+                elif key == 'created_at' or key == 'updated_at':
+                    setattr(self, key,
+                            datetime.strptime(value, date_format))
+                else:
+                    setattr(self, key, value)
+
+        models.storage.new(self)
 
     def __str__(self):
-        """ return string represitnation of an object"""
-        cls = self.__class__.__name__
-        return "[{}] ({}) {}".format(cls, self.id, self.__dict__)
+        """ print: [<class name>] (<self.id>) <self.__dict__>"""
+
+        return "[{}] ({}) {}".format(self.__class__.__name__,
+                                     self.id, (self.__dict__))
 
     def save(self):
-        """ updates the attribute updated_at with the current datetime"""
-        self.updated_at = datetime.now()
+        """updates the public instance attribute
+        updated_at with the current datetime
+        """
+
+        self.updated_at = datetime.utcnow()
+        models.storage.save()
 
     def to_dict(self):
-        """returns a dictionary containing all keys/values of __dict__"""
-        dict_copy = self.__dict__.copy()
-        dict_copy["created_at"] = self.created_at.isoformat()
-        dict_copy["updated_at"] = self.updated_at.isoformat()
-        dict_copy["__class__"] = self.__class__.__name__
-        return dict_copy
+        """returns a dictionary containing all keys/values
+        of __dict__ of the instance
+        """
+
+        dict_user = self.__dict__.copy()
+        dict_user["created_at"] = self.created_at.isoformat()
+        dict_user["updated_at"] = self.updated_at.isoformat()
+        dict_user['__class__'] = self.__class__.__name__
+        return dict_user
